2f960ccd2089c1deef72c9e760c8166126e98f99
khalifardy <khalifardy.miqdarsah@gmail.com>
2024-01-02 18:52:02 +0700

    finish

--- .next/static/aPWuKWir7q28mQV56Ne7r/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/121-c8f1b357e5fdf181.js","static/chunks/pages/index-8ba7f11fc2f62384.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
